d5ccaa4c6d70a7801627b503730bef507daa18fe
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-01-04 19:56:11 +0600

    modifi category model

--- controllers/Category.controller.js
+++ controllers/Category.controller.js
@@ -1,42 +1,46 @@
 const CategoryModel = require("../models/Category.model");
-const Format = require('response-format');
+const Format = require("response-format");
 
 async function getCategorys(req, res, next) {
-    try {
-        const categorys = await CategoryModel.find({})
-        res.status(200).json(categorys)
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const categorys = await CategoryModel.find({});
+    res.status(200).json(categorys);
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function getCategory(req, res, next) {
-    try {
-        const { categoryId } = req.params || {};
-        const category = await CategoryModel.findById(categoryId)
-        res.status(200).json(category)
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const { categoryId } = req.params || {};
+    const category = await CategoryModel.findById(categoryId);
+    res.status(200).json(category);
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function addCategory(req, res, next) {
-    try {
-        console.log(req.body)
-        const { name } = req.body
-        const newCategory = new CategoryModel({
-            name
-        })
+  try {
+    const { name, icon, featured } = req.body || {};
 
-        await newCategory.save()
+    const newCategory = new CategoryModel({
+      name,
+      icon,
+      featured: featured ? featured : false,
+    });
 
-        res.json(Format.success(`${name} category add successful`, newCategory))
-    } catch (error) {
-        console.log(error)
-        next(error)
-    }
+    await newCategory.save();
+
+    res.json(Format.success(`${name} category add successful`, newCategory));
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 }
 
 module.exports = {
-    getCategory, getCategorys, addCategory
-}
+  getCategory,
+  getCategorys,
+  addCategory,
+};
--- models/Category.model.js
+++ models/Category.model.js
@@ -1,13 +1,23 @@
 const { Schema, default: mongoose } = require("mongoose");
 
-const categorySchema = new Schema({
+const categorySchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const CategoryModel = mongoose.model("category", categorySchema);
 
-module.exports = CategoryModel
-
+module.exports = CategoryModel;
